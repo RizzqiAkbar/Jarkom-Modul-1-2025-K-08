6b0ade1bb57995fabc2a76c98d4b2791cefa2185
Adinda Cahya Pramesti <eh.adindacahya@gmail.com>
2025-10-01 23:45:15 +0700

    Update soal_8.sh

--- script/soal_8.sh
+++ script/soal_8.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#ini di ulmo ya best
+apt update
+apt install unzip
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=11ra_yTV_adsPIXeIPMSt0vrxCBZu0r33" -O cuaca.zip && unzip cuaca.zip
 
+service vsftpd start
+ftp 192.215.1.1
