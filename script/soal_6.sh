9ea1da78cb536f5126eb6bf05cf960bc720e2c7c
Adinda Cahya Pramesti <eh.adindacahya@gmail.com>
2025-10-01 23:26:29 +0700

    Update soal_6.sh

--- script/soal_6.sh
+++ script/soal_6.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+apt update
+apt install unzip -y
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1bE3kF1Nclw0VyKq4bL2VtOOt53IC7lG5" -O traffic.zip && unzip\302\240traffic.zip
+chmod +x traffic.sh
+./traffic.sh
 
