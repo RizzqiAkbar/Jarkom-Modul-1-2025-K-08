08160944e5764e6690d55c6ba652f9c9d8dd7a99
Adinda Cahya Pramesti <eh.adindacahya@gmail.com>
2025-10-01 23:35:56 +0700

    Update soal_12.sh

--- script/soal_12.sh
+++ script/soal_12.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# login ke melkorrr
 
+
+apt update
+apt install netcat
+apt install vsftpd -y
+service vsftpd start
+apt install apache2 -y
+service apache2 start
+nc -zv 192.215.1.2 21
+nc -zv 192.215.1.2 80
+nc -zv 192.215.1.2 666
