a06d2410cd56281bfb423d31b6fd76b90290e5e4
Adinda Cahya Pramesti <eh.adindacahya@gmail.com>
2025-10-01 23:53:31 +0700

    Update soal_4.sh

--- script/soal_4.sh
+++ script/soal_4.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# configure dulu ke setiap client nya
+# di tiap client kalo udh do configure lgsg test pake
+
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+
+ping google.com
 
