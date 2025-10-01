68174a39d69ac1a4254e8e48ce3d29a47df5fb6a
Adinda Cahya Pramesti <eh.adindacahya@gmail.com>
2025-10-01 23:47:20 +0700

    Update soal_9.sh

--- script/soal_9.sh
+++ script/soal_9.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+# eru
+apt update
+apt install unzip
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=11ua2KgBu3MnHEIjhBnzqqv2RMEiJsILY" -O kitab_penciptaan.zip && unzip kitab_penciptaan.zip
+service vsftpd restart
+ftp 192.215.1.1
