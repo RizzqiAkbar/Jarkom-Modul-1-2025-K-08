e182c437c264caaf6507e1c1466821f6399a8034
Adinda Cahya Pramesti <eh.adindacahya@gmail.com>
2025-10-01 23:45:02 +0700

    Update soal_9.sh

--- script/soal_9.sh
+++ script/soal_9.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-#ini di ulmo ya best
-apt update
-apt install unzip
-wget --no-check-certificate "https://drive.google.com/uc?export=download&id=11ra_yTV_adsPIXeIPMSt0vrxCBZu0r33" -O cuaca.zip && unzip cuaca.zip
 
-service vsftpd start
-ftp 192.215.1.1
